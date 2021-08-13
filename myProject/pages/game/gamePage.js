57b96134b069690cc1831a7cf21b406158b5c017
katerina-yarm <katerina.tatarevich@gmail.com>
2021-08-13 15:59:54 +0300

    sprite animation

--- myProject/pages/game/gamePage.js
+++ myProject/pages/game/gamePage.js
@@ -1,7 +1,8 @@
 function gamePageLoading (){
     let gameHtml = `<div class="mainPage">
             <a href="#Main" class="fciA navItem" onclick="switchToMainPage()"><span class="fciSpan">Back to main page</span></a>
-            <div>Здесь будет игра</div>
+            <div class='bird'></div>
+            <div class='coin'></div>
         </div>`;
     return gameHtml;
 }
