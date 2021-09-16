365d3a3845b2c838e4dc86b1465e6a0e8213a049
katerina-yarm <katerina.tatarevich@gmail.com>
2021-09-16 18:11:17 +0300

    sound settings

--- myProject/pages/main/mainPage.js
+++ myProject/pages/main/mainPage.js
@@ -3,12 +3,30 @@ function mainPageLoading (){
     let ctx = document.getElementById('game').getContext('2d');
     ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
 
+    function sound(){
+        if (soundOn==true){
+            if(soundOn==true){
+                buttonClick.play();
+            }
+            fonAudio.pause();
+            fonAudio.currentTime = 0;
+            return soundOn=false;
+        }
+        if (soundOn==false){
+            if(soundOn==true){
+                buttonClick.play();
+            }
+            fonAudio.play();
+            return soundOn=true;
+        }
+    }
     let MainHtml = `   
         <div class="mainPage">
             <img src='assets/rocket.png'>
             <a href="#Game" class="fciA navItem" onclick="switchToGamePage()"><span class="fciSpan">New Game</span></a>
             <a href="#Rules" class="fciA navItem" onclick="switchToRulesPage()"><span class="fciSpan">Rules</span></a>
             <a href="#Records" class="fciA navItem" onclick="switchToRecordsPage()"><span class="fciSpan">Scores</span></a>
+            <a href="#Records" class="fciA navItem" onclick="sound()"><span class="fciSpan">On/Off sound</span></a>
         </div>`;
     return MainHtml;
 }
