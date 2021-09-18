90583b98953a45be8e537556239b57b737991d96
katerina-yarm <katerina.tatarevich@gmail.com>
2021-09-18 16:38:47 +0300

    page about was added

--- myProject/pages/about/aboutPage.js
+++ myProject/pages/about/aboutPage.js
@@ -1,16 +1,19 @@
-function mainPageLoading (){
+function aboutPageLoading (){
     //очищаем холст, чтобы не отображался финальный кадр игры
     let ctx = document.getElementById('game').getContext('2d');
     ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
 
-    let MainHtml = `   
-        <div class="mainPage">
-            <img src='assets/rocket.png'>
-            <a href="#About" class="fciA navItem" onclick="switchToAboutPage()"><span class="fciSpan">About</span></a>
+    let aboutHtml = `   
+        <div class="rulesPage">
             <a href="#Game" class="fciA navItem" onclick="switchToGamePage()"><span class="fciSpan">New Game</span></a>
+            <a href="#Main" class="fciA navItem" onclick="switchToMainPage()"><span class="fciSpan">Main page</span></a>
             <a href="#Rules" class="fciA navItem" onclick="switchToRulesPage()"><span class="fciSpan">Rules</span></a>
             <a href="#Records" class="fciA navItem" onclick="switchToRecordsPage()"><span class="fciSpan">Scores</span></a>
-            <a class="fciA navItem" onclick="sound()"><span class="fciSpan">Sound On/Off</span></a>
+        </div>;
+        <div class="rules">
+            <h3>"The Rocket Wars"</h3>
+            <br>
+            The development of this project included the usage of HTML5, CSS, JavaScript, Canvas, AJAX, JQuery.
         </div>`;
-    return MainHtml;
+    return aboutHtml;
 }
