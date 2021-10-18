dbaa4b15da73c353a8881f445892cf95f999b5cb
katerina-yarm <katerina.tatarevich@gmail.com>
2021-10-18 13:07:59 +0300

    language changes are working

---
+++ toDoList/lang.js
@@ -0,0 +1,74 @@
+const langArr = {
+    'header':{
+        'ru':'Мои списки дел',
+        'en':'List of my deals'
+    },
+    'theme':{
+        'ru':'Изменить тему',
+        'en':'Change theme'
+    },
+    'add':{
+        'ru':'Добавить новую запись',
+        'en':'Add new todo'
+    },
+    'sort':{
+        'ru':'Сортировать',
+        'en':'Sort'
+    },
+    'date':{
+        'ru':'по дате',
+        'en':'by date'
+    },
+    'prior':{
+        'ru':'по приоритету',
+        'en':'by priority'
+    },
+    'delete':{
+        'ru':'Удалить',
+        'en':'Delete'
+    },
+    'change':{
+        'ru':'Изменить',
+        'en':'Change'
+    },
+    'changeTitle':{
+        'ru':'Здесь вы можете внести изменения в заметку',
+        'en':'Here you can make changes with the note'
+    },
+    'newNoteTitle':{
+        'ru':'Внесите новую заметку в ваш список дел',
+        'en':'Add a new note to your todo list'
+    },
+    'title':{
+        'ru':'Название',
+        'en':'Title'
+    },
+    'priority':{
+        'ru':'Приоритет',
+        'en':'Priority'
+    },
+    'lowPriority':{
+        'ru':'Низкий',
+        'en':'Low'
+    },
+    'middlePriority':{
+        'ru':'Средний',
+        'en':'Middle'
+    },
+    'highPriority':{
+        'ru':'Высокий',
+        'en':'High'
+    },
+    'endDate':{
+        'ru':'Дата окончания',
+        'en':'End date'
+    },
+    'description':{
+        'ru':'Описание',
+        'en':'Description'
+    },
+    'saveChanges':{
+        'ru':'Сохранить изменения',
+        'en':'Save changes'
+    }
+}
--- toDoList/script.js
+++ toDoList/script.js
@@ -1,3 +1,9 @@
+    //создадим переменную для смены языка
+    let languageChange='en';
+    function languageChangeButton(language){
+        languageChange=language;
+        showToDos();
+    }
 
     const app = document.getElementById('app');
 
@@ -40,8 +46,8 @@
                 <p class="card-date">${todos[i].endDate}</p>
                 <p class="card-priority">${todos[i].priority}</p>
                 <p class="card-description">${todos[i].description}</p>
-                <button data-todoid=${todos[i].id} type="button" class='btn-del'>Удалить</button>
-                <button type="button" onclick='showChangesForm(${todos[i].id})'>Изменить</button> 
+                <button data-todoid=${todos[i].id} type="button" class='btn-del lng-delete'>Удалить</button>
+                <button type="button" onclick='showChangesForm(${todos[i].id})' class='lng-change'>Изменить</button> 
             </div>`
             toDoNum++;
         }
@@ -51,6 +57,7 @@
     // отображает все записи + позволяет работать с ними (т.е. внутри функция для удаления записи)
     function showToDos () {
         app.innerHTML=render();
+        changeLanguage(languageChange);
 
         let buttons = document.querySelectorAll('.btn-del'); //выбирает в массив все кнопки класса '.btn-del'
         buttons.forEach ((btn,indx) => {// назначим обработчик по кнопке, он будет вызван, когда кликнут на любую кнопку
@@ -79,16 +86,17 @@
         document.body.appendChild(form).classList = "form formStyle";
         let index=todos.findIndex(i=>i.id==modalButtonId)
         form.innerHTML = changesRender(index);
+        changeLanguage(languageChange);
     }
 
     //функция render для отображения полей заметки, которую нужно откорректировать
     function changesRender(i){
         let modalHTML ="";
             modalHTML += `  
-                    <h5>Здесь вы можете внести изменения в заметку</h5>
+                    <h5 class="lng-changeTitle">Здесь вы можете внести изменения в заметку</h5>
                     <div class="modal-body">
                         <div>
-                            <label>Название</label>
+                            <label class="lng-title">Название</label>
                             <input id="name" value="${todos[i].name}">
                         </div>
                         <div>
@@ -96,20 +104,20 @@
                             <input id="id" value="${todos[i].id}" readonly>
                         </div>
                         <div>
-                            <label>Приоритет</label>
+                            <label class="lng-priority">Приоритет</label>
                             <input id="priority" value="${todos[i].priority}">
                         </div>
                         <div>
-                            <label>Дата окончания</label>
+                            <label class="lng-endDate">Дата окончания</label>
                             <input id="endDate" type='date' value="${todos[i].endDate}">
                         </div>
                         <div>
-                            <label>Описание</label>
+                            <label class="lng-description">Описание</label>
                             <input id="description" value="${todos[i].description}">
                         </div>
                     </div>
                     <div>
-                    <button type="button" onclick='getChangedValues(${i})'>Сохранить изменения</button>
+                    <button type="button" onclick='getChangedValues(${i})' class="lng-saveChanges">Сохранить изменения</button>
                     </div>`;
             return modalHTML;
     }
@@ -140,16 +148,17 @@
         let newToDo = document.createElement('form');
         document.body.appendChild(newToDo).classList = "newToDo formStyle";
         newToDo.innerHTML = newToDoRender();
+        changeLanguage(languageChange);
     }
 
     //функция render для отображения полей для заполнения данных по новой заметке
     function newToDoRender(){
         let modalHTML ="";
             modalHTML += `  
-                    <h5>Внесите новую заметку в ваш список дел</h5>
+                    <h5 class="lng-newNoteTitle">Внесите новую заметку в ваш список дел</h5>
                     <div class="modal-body">
                         <div>
-                            <label>Название</label>
+                            <label class="lng-title">Название</label>
                             <input id="newName" placeholder='Развлечение'>
                         </div>
                         <div>
@@ -157,22 +166,22 @@
                             <input id="newId" placeholder='5'>
                         </div>
                         <div>
-                            <label>Приоритет</label>
+                            <label class="lng-priority">Приоритет</label>
                             <select id="newPriority">
-                                <option value="Низкий" selected>Низкий</option>
-                                <option value="Средний">Средний</option>
-                                <option value="Высокий">Высокий</option>
+                                <option value="Низкий" selected class="lng-lowPriority">Низкий</option>
+                                <option value="Средний" class="lng-middlePriority">Средний</option>
+                                <option value="Высокий" class="lng-highPriority">Высокий</option>
                             </select>
                         <div>
-                            <label>Дата окончания</label>
+                            <label class="lng-endDate">Дата окончания</label>
                             <input id="newEndDate" type='date'>
                         </div>
                         <div>
-                            <label>Описание</label>
+                            <label class="lng-description">Описание</label>
                             <input id="newDescription" placeholder='Сходить в театр'>
                         </div>
                     </div>
-                    <button type="button" onclick='getNewValues()'>Сохранить изменения</button>`;
+                    <button type="button" onclick='getNewValues()' class="lng-saveChanges">Сохранить изменения</button>`;
             return modalHTML;
     }
     
@@ -248,6 +257,14 @@
         localStorage.setItem (`todos`, JSON.stringify(todos));
         showToDos();
     }
-    
 
+    //изменяем язык сайта
+    function changeLanguage (language){
+        for(let key in langArr){
+            let elem = document.querySelectorAll(`.lng-${key}`)
+            for(let i=0; i<elem.length; i++){
+                elem[i].innerHTML=langArr[key][language]
+            }
+        }
+    }
     
